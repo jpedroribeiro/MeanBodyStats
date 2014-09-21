a6f81db0256c229235a5975c53fcdb4067125709
Joao pedro Accorsi ribeiro <jpedroribeiro@joaopar-air.local>
2014-09-21 16:29:46 +0100

    added temp solution for auto load feature

--- client/app.js
+++ client/app.js
@@ -18,8 +18,7 @@ bodyStats.factory("MainFactory", function($http){
         // Set/Update DB with new data
         insertNewProfile: function(newEntry) {
             $http.post("http://localhost:3000/save", newEntry).success(function(){
-               console.log('boom');
-                // Todo: do something here
+                // TODO: confirmation message?
             });
         }
 
@@ -37,41 +36,76 @@ bodyStats.factory("MainFactory", function($http){
  */
 bodyStats.controller("MainController", function($http, $scope, MainFactory){
 
-    // Initialize app
+    // Blank form
+    var emptyForm = {
+        _id: '',
+        name: '',
+        entry: {
+            date: '',
+            weight: '',
+            fat_chest: '',
+            fat_abdominal: '',
+            fat_thigh: '',
+            fat_triceps: '',
+            fat_subscapular: '',
+            fat_suprailiac: '',
+            fat_midaxillary: '',
+            neck: '',
+            shoulders: '',
+            chest: '',
+            biceps: '',
+            waist: '',
+            hips: '',
+            thigh: ''
+        }
+    };
+
+    // Loads data
     MainFactory.getData().success(function(result){
         $scope.profiles = result;
     });
+
+    // temporary solution for auto load new inserted results
+    $scope.refresh = function(){
+        // Loads data
+        MainFactory.getData().success(function(result){
+            $scope.profiles = result;
+        });
+    }
 
 
     // Inserts or updates profile
     $scope.insertData = function(){
 
         var newEntry = {
-            _id: $scope.newId,
-            name: $scope.newName,
+            _id: $scope.entry.newId,
+            name: $scope.entry.newName,
             entry: {
-                date: $scope.newDate,
-                weight: $scope.newWeight,
-                fat_chest: $scope.newFat_chest,
-                fat_abdominal: $scope.newFatAbdominal,
-                fat_thigh: $scope.newFatThigh,
-                fat_triceps: $scope.newFatTriceps,
-                fat_subscapular: $scope.newFatSubscapular,
-                fat_suprailiac: $scope.newFatSuprailiac,
-                fat_midaxillary: $scope.newFatMidaxillary,
-                neck: $scope.newNeck,
-                shoulders: $scope.newShoulders,
-                chest: $scope.newChest,
-                biceps: $scope.newBiceps,
-                waist: $scope.newWaist,
-                hips: $scope.newHips,
-                thigh: $scope.newThigh
+                date: $scope.entry.newDate,
+                weight: $scope.entry.newWeight,
+                fat_chest: $scope.entry.newFat_chest,
+                fat_abdominal: $scope.entry.newFatAbdominal,
+                fat_thigh: $scope.entry.newFatThigh,
+                fat_triceps: $scope.entry.newFatTriceps,
+                fat_subscapular: $scope.entry.newFatSubscapular,
+                fat_suprailiac: $scope.entry.newFatSuprailiac,
+                fat_midaxillary: $scope.entry.newFatMidaxillary,
+                neck: $scope.entry.newNeck,
+                shoulders: $scope.entry.newShoulders,
+                chest: $scope.entry.newChest,
+                biceps: $scope.entry.newBiceps,
+                waist: $scope.entry.newWaist,
+                hips: $scope.entry.newHips,
+                thigh: $scope.entry.newThigh
             }
         };
 
         MainFactory.insertNewProfile(newEntry);
 
-       //TODO: reload
+        // clears form
+        $scope.entry = emptyForm;
+
+
     }
 
 });
--- server/routes.js
+++ server/routes.js
@@ -8,28 +8,37 @@ module.exports = function(server, Profile){
     });
 
     server.post('/save', function(req, res){
+
+        // Reads input and stringifies it
         var content = '';
         req.on("data",function(chunk){
             content = chunk.toString();
         });
+
+        // Saves or Updates profile
         req.on("end",function(){
             var parsedContent = JSON.parse(content);
             Profile.findOne({ '_id': parsedContent['_id']}, function(err, resultProfile){
                 if( err ) {
-                    console.log("Error found: " + err );
+                    console.log("Error found when trying to find profile: " + err );
                     return;
                 }
                 if ( !resultProfile ) {
                     console.log("Profile (_id) not found, creating one...");
                     var mongoose = require('mongoose');
+                    // Generates mongoose style id for this new profile
                     parsedContent["_id"] = mongoose.Types.ObjectId();
                     resultProfile = new Profile(parsedContent);
                 }
+                /**
+                 *  Updates (or sets) properties of the existing (or new) profile
+                 *  Entries are pushed as a whole set, in the future, find a way of updating single measurements in 'entries' (TODO)
+                 */
                 resultProfile.name = parsedContent['name'];
                 resultProfile.entries.push(parsedContent['entry']);
                 resultProfile.save( function ( err, resultProfile ){
                     if( err ) {
-                        console.log( err );
+                        console.log("Error found when trying to save profile: " +  err );
                         return;
                     }
                     res.status(200);
